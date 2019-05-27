187b07af0ebc49b25fc1cdd5dcacd8f31a485ee4
daichitakahashi <daichitakahashi@DIQ-002.local>
2019-05-27 12:06:26 +0900

    Created New function and add descriptions

--- oncewait.go
+++ oncewait.go
@@ -2,19 +2,27 @@ package oncewait
 
 import "sync"
 
-// OnceWaiter is
+// OnceWaiter is an object that will perform exactly one action.
+// But wait for finish of first Do.
 type OnceWaiter struct {
 	once      sync.Once
 	completed chan struct{}
 }
 
-// Do is
+// Do calls the function f f and only if Do is being called for the first time
+// for this instance of OnceWaiter, same as standard package's sync.Once.
+// While function f is executing, second call waits for finish.
 func (o *OnceWaiter) Do(f func()) {
 	o.once.Do(func() {
 		f()
 		close(o.completed)
 	})
 	<-o.completed
+}
+
+// New returns fresh OnceWaiter.
+func New() *OnceWaiter {
+	return &OnceWaiter{completed: make(chan struct{})}
 }
 
 // Factory is
